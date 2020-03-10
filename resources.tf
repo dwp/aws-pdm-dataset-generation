c33dab941300f1cd7ce774b85badb7ec4af58c08
Gordon Adam <gordon.adam@infinityworks.com>
2020-03-10 15:50:19 +0000

    Adding security groups

--- resources.tf
+++ resources.tf
@@ -114,3 +114,40 @@ resource "aws_iam_role_policy_attachment" "ec2_for_ssm_attachment" {
   role       = aws_iam_role.pdm_dataset_generator.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
 }
+
+resource "aws_security_group" "pdm_dataset_generation_service" {
+  name                   = "pdm_dataset_generation_service"
+  description            = "Contains rules automatically added by the EMR service itself. See https://docs.aws.amazon.com/emr/latest/ManagementGuide/emr-man-sec-groups.html#emr-sg-elasticmapreduce-sa-private"
+  revoke_rules_on_delete = true
+  vpc_id                 = data.terraform_remote_state.internal_compute.outputs.vpc.id 
+}
+
+resource "aws_security_group" "pdm_dataset_generation" {
+  name                   = "pdm_dataset_generation_common"
+  description            = "Contains rules for both EMR cluster master nodes and EMR cluster slave nodes"
+  revoke_rules_on_delete = true
+  vpc_id                 = data.terraform_remote_state.internal_compute.outputs.vpc.id 
+}
+
+
+resource "aws_security_group_rule" "pdm_dataset_generation_egress" {
+  description       = "Allow outbound traffic from PDM Dataset Generation EMR Cluster"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  #prefix_list_ids   = [module.vpc.s3_prefix_list_id]
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.pdm_dataset_generation.id
+}
+
+resource "aws_security_group_rule" "pdm_dataset_generation_ingress" {
+  description       = "Allow inbound traffic from PDM Dataset Generation EMR Cluster"
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  #prefix_list_ids   = [module.vpc.s3_prefix_list_id]
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.pdm_dataset_generation.id
+}
