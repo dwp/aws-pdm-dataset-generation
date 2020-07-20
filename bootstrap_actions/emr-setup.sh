19212a01f3bc38773b9ebccbdc279e6e0eba40f4
Levan Kirvalidze <levanik@live.com>
2020-07-20 16:32:10 +0100

    fixing bugs in emr-setup bootsrtap action

--- bootstrap_actions/emr-setup.sh
+++ bootstrap_actions/emr-setup.sh
@@ -21,49 +21,49 @@ chmod u+x /opt/emr/logging.sh
 chmod u+x /opt/emr/cloudwatch.sh
 
 (
-    # Import the logging functions
-    source /opt/emr/logging.sh
-    
-    function log_wrapper_message() {
-        log_pdm_message "$${1}" "emr-setup.sh" "$${PID}" "$${@:2}" "Running as: ,$USER"
-    }
-    
-    log_wrapper_message "Setting up the Proxy"
-    
-    echo -n "Running as: "
-    whoami
-    
-    export AWS_DEFAULT_REGION=${aws_default_region}
-    
-    FULL_PROXY="${full_proxy}"
-    FULL_NO_PROXY="${full_no_proxy}"
-    export http_proxy="$FULL_PROXY"
-    export HTTP_PROXY="$FULL_PROXY"
-    export https_proxy="$FULL_PROXY"
-    export HTTPS_PROXY="$FULL_PROXY"
-    export no_proxy="$FULL_NO_PROXY"
-    export NO_PROXY="$FULL_NO_PROXY"
-    export PDM_LOG_LEVEL="${PDM_LOG_LEVEL}"
-    
-    echo "Setup cloudwatch logs"
-    sudo /opt/emr/cloudwatch.sh \
+# Import the logging functions
+source /opt/emr/logging.sh
+
+function log_wrapper_message() {
+    log_pdm_message "$${1}" "emr-setup.sh" "$${PID}" "$${@:2}" "Running as: ,$USER"
+}
+
+log_wrapper_message "Setting up the Proxy"
+
+echo -n "Running as: "
+whoami
+
+export AWS_DEFAULT_REGION=${aws_default_region}
+
+FULL_PROXY="${full_proxy}"
+FULL_NO_PROXY="${full_no_proxy}"
+export http_proxy="$FULL_PROXY"
+export HTTP_PROXY="$FULL_PROXY"
+export https_proxy="$FULL_PROXY"
+export HTTPS_PROXY="$FULL_PROXY"
+export no_proxy="$FULL_NO_PROXY"
+export NO_PROXY="$FULL_NO_PROXY"
+export PDM_LOG_LEVEL="${PDM_LOG_LEVEL}"
+
+echo "Setup cloudwatch logs"
+sudo /opt/emr/cloudwatch.sh \
     "${cwa_metrics_collection_interval}" "${cwa_namespace}"  "${cwa_log_group_name}" \
     "${aws_default_region}" "${cwa_bootstrap_loggrp_name}" "${cwa_steps_loggrp_name}" \
     "${cwa_yarnspark_loggrp_name}"
-    
-    export ACM_KEY_PASSWORD=$(uuidgen -r)
-    
-    log_wrapper_message "Getting the DKS Certificate Details "
-    
-    ## get dks cert
-    export TRUSTSTORE_PASSWORD=$(uuidgen -r)
-    export KEYSTORE_PASSWORD=$(uuidgen -r)
-    export PRIVATE_KEY_PASSWORD=$(uuidgen -r)
-    export ACM_KEY_PASSWORD=$(uuidgen -r)
-    
-    #sudo mkdir -p /opt/emr
-    #sudo chown hadoop:hadoop /opt/emr
-    touch /opt/emr/dks.properties
+
+export ACM_KEY_PASSWORD=$(uuidgen -r)
+
+log_wrapper_message "Getting the DKS Certificate Details "
+
+## get dks cert
+export TRUSTSTORE_PASSWORD=$(uuidgen -r)
+export KEYSTORE_PASSWORD=$(uuidgen -r)
+export PRIVATE_KEY_PASSWORD=$(uuidgen -r)
+export ACM_KEY_PASSWORD=$(uuidgen -r)
+
+#sudo mkdir -p /opt/emr
+#sudo chown hadoop:hadoop /opt/emr
+touch /opt/emr/dks.properties
 cat >> /opt/emr/dks.properties <<EOF
 identity.store.alias=${private_key_alias}
 identity.key.password=$PRIVATE_KEY_PASSWORD
@@ -75,10 +75,10 @@ trust.keystore=/opt/emr/truststore.jks
 trust.store.password=$TRUSTSTORE_PASSWORD
 data.key.service.url=${dks_endpoint}
 EOF
-    
-    log_wrapper_message "Retrieving the ACM Certificate details"
-    
-    /usr/local/bin/acm-cert-retriever \
+
+log_wrapper_message "Retrieving the ACM Certificate details"
+
+/usr/local/bin/acm-cert-retriever \
     --acm-cert-arn "${acm_cert_arn}" \
     --acm-key-passphrase "$ACM_KEY_PASSWORD" \
     --keystore-path "/opt/emr/keystore.jks" \
@@ -90,107 +90,24 @@ EOF
     --truststore-aliases "${truststore_aliases}" \
     --truststore-certs "${truststore_certs}" \
     --jks-only true >> /var/log/pdm/acm-cert-retriever.log 2>&1
-    
-    
-    sudo -E /usr/local/bin/acm-cert-retriever \
+
+
+sudo -E /usr/local/bin/acm-cert-retriever \
     --acm-cert-arn "${acm_cert_arn}" \
     --acm-key-passphrase "$ACM_KEY_PASSWORD" \
     --private-key-alias "${private_key_alias}" \
     --truststore-aliases "${truststore_aliases}" \
     --truststore-certs "${truststore_certs}"  >> /var/log/pdm/acm-cert-retriever.log 2>&1
-    
-    cd /etc/pki/ca-trust/source/anchors/
-    sudo touch pdm_ca.pem
-    sudo chown hadoop:hadoop /etc/pki/tls/private/"${private_key_alias}".key /etc/pki/tls/certs/"${private_key_alias}".crt /etc/pki/ca-trust/source/anchors/pdm_ca.pem
-    TRUSTSTORE_ALIASES="${truststore_aliases}"
-    for F in $(echo $TRUSTSTORE_ALIASES | sed "s/,/ /g"); do
-        (sudo cat "$F.crt"; echo) >> pdm_ca.pem;
-    done
-    
-    
-    log_wrapper_message "Completed the emr-setup.sh step of the EMR Cluster"
-    
-) >> /var/log/pdm/nohup.log 2>&1
-    log_wrapper_message "Setting up the Proxy"
-    
-    echo -n "Running as: "
-    whoami
-    
-    export AWS_DEFAULT_REGION=${aws_default_region}
-    
-    FULL_PROXY="${full_proxy}"
-    FULL_NO_PROXY="${full_no_proxy}"
-    export http_proxy="$FULL_PROXY"
-    export HTTP_PROXY="$FULL_PROXY"
-    export https_proxy="$FULL_PROXY"
-    export HTTPS_PROXY="$FULL_PROXY"
-    export no_proxy="$FULL_NO_PROXY"
-    export NO_PROXY="$FULL_NO_PROXY"
-    export PDM_LOG_LEVEL="${PDM_LOG_LEVEL}"
-    
-    echo "Setup cloudwatch logs"
-    sudo /opt/emr/cloudwatch.sh \
-    "${cwa_metrics_collection_interval}" "${cwa_namespace}"  "${cwa_log_group_name}" \
-    "${aws_default_region}" "${cwa_bootstrap_loggrp_name}" "${cwa_steps_loggrp_name}" \
-    "${cwa_yarnspark_loggrp_name}"
-    
-    export ACM_KEY_PASSWORD=$(uuidgen -r)
-    
-    log_wrapper_message "Getting the DKS Certificate Details "
-    
-    ## get dks cert
-    export TRUSTSTORE_PASSWORD=$(uuidgen -r)
-    export KEYSTORE_PASSWORD=$(uuidgen -r)
-    export PRIVATE_KEY_PASSWORD=$(uuidgen -r)
-    export ACM_KEY_PASSWORD=$(uuidgen -r)
-    
-    #sudo mkdir -p /opt/emr
-    #sudo chown hadoop:hadoop /opt/emr
-    touch /opt/emr/dks.properties
-cat >> /opt/emr/dks.properties <<EOF
-identity.store.alias=${private_key_alias}
-identity.key.password=$PRIVATE_KEY_PASSWORD
-spark.ssl.fs.enabled=true
-spark.ssl.keyPassword=$KEYSTORE_PASSWORD
-identity.keystore=/opt/emr/keystore.jks
-identity.store.password=$KEYSTORE_PASSWORD
-trust.keystore=/opt/emr/truststore.jks
-trust.store.password=$TRUSTSTORE_PASSWORD
-data.key.service.url=${dks_endpoint}
-EOF
-    
-    log_wrapper_message "Retrieving the ACM Certificate details"
-    
-    /usr/local/bin/acm-cert-retriever \
-    --acm-cert-arn "${acm_cert_arn}" \
-    --acm-key-passphrase "$ACM_KEY_PASSWORD" \
-    --keystore-path "/opt/emr/keystore.jks" \
-    --keystore-password "$KEYSTORE_PASSWORD" \
-    --private-key-alias "${private_key_alias}" \
-    --private-key-password "$PRIVATE_KEY_PASSWORD" \
-    --truststore-path "/opt/emr/truststore.jks" \
-    --truststore-password "$TRUSTSTORE_PASSWORD" \
-    --truststore-aliases "${truststore_aliases}" \
-    --truststore-certs "${truststore_certs}" \
-    --jks-only true >> /var/log/pdm/acm-cert-retriever.log 2>&1
-    
-    
-    sudo -E /usr/local/bin/acm-cert-retriever \
-    --acm-cert-arn "${acm_cert_arn}" \
-    --acm-key-passphrase "$ACM_KEY_PASSWORD" \
-    --private-key-alias "${private_key_alias}" \
-    --truststore-aliases "${truststore_aliases}" \
-    --truststore-certs "${truststore_certs}"  >> /var/log/pdm/acm-cert-retriever.log 2>&1
-    
-    cd /etc/pki/ca-trust/source/anchors/
-    sudo touch pdm_ca.pem
-    sudo chown hadoop:hadoop /etc/pki/tls/private/"${private_key_alias}".key /etc/pki/tls/certs/"${private_key_alias}".crt /etc/pki/ca-trust/source/anchors/pdm_ca.pem
-    TRUSTSTORE_ALIASES="${truststore_aliases}"
-    for F in $(echo $TRUSTSTORE_ALIASES | sed "s/,/ /g"); do
-        (sudo cat "$F.crt"; echo) >> pdm_ca.pem;
-    done
-    
-    
-    log_wrapper_message "Completed the emr-setup.sh step of the EMR Cluster"
-    
+
+cd /etc/pki/ca-trust/source/anchors/
+sudo touch pdm_ca.pem
+sudo chown hadoop:hadoop /etc/pki/tls/private/"${private_key_alias}".key /etc/pki/tls/certs/"${private_key_alias}".crt /etc/pki/ca-trust/source/anchors/pdm_ca.pem
+TRUSTSTORE_ALIASES="${truststore_aliases}"
+for F in $(echo $TRUSTSTORE_ALIASES | sed "s/,/ /g"); do
+ (sudo cat "$F.crt"; echo) >> pdm_ca.pem;
+done
+
+
+log_wrapper_message "Completed the emr-setup.sh step of the EMR Cluster"
+
 ) >> /var/log/pdm/nohup.log 2>&1
