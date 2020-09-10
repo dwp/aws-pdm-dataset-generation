6d6a2cfce64b15c0a229f0303e509e17b33cf3a8
abdurrahmannuhu <abdurrahman.nuhu@dwp.gsi.gov.ukgit config --global user.name abdurrahmannuhugit config --global user.email abdurrahman.nuhu@dwp.gsi.gov.uk>
2020-09-10 16:26:16 +0100

    added script and step for creating databases

--- cluster_config/steps.yaml.tpl
+++ cluster_config/steps.yaml.tpl
@@ -22,6 +22,12 @@ Steps:
     - "s3://${s3_config_bucket}/component/pdm-dataset-generation/addresses_tagged.sh"
     Jar: "s3://eu-west-2.elasticmapreduce/libs/script-runner/script-runner.jar"
   ActionOnFailure: "CONTINUE"
+- Name: "create-databases"
+  HadoopJarStep:
+    Args:
+    - "s3://${s3_config_bucket}/component/pdm-dataset-generation/create_db.sh"
+    Jar: "s3://eu-west-2.elasticmapreduce/libs/script-runner/script-runner.jar"
+  ActionOnFailure: "CONTINUE"
 - Name: "transactional"
   HadoopJarStep:
     Args:
---
+++ steps/create_db.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+###############
+# Set Variables
+###############
+
+
+(
+ # Import the logging functions
+    source /opt/emr/logging.sh
+
+    function log_wrapper_message() {
+        log_pdm_message "$${1}" "create_db_sql.sh" "$${PID}" "$${@:2}" "Running as: ,$USER"
+    }
+
+    echo "START_RUNNING_CREATE_DB ......................"
+    log_wrapper_message "start running create db ......................."
+    #####################
+    # Run SQL Scripts
+    #####################
+
+    hive -f /opt/emr/sql/extracted/src/main/resources/scripts/db/create_db.sql
+ 
+    echo "FINISHED_RUNNING_CREATE_DB......................"
+    log_wrapper_message "finished running create db ......................."
+
+) >> /var/log/pdm/create_db_sql.log 2>&1
