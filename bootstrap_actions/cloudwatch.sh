7389780bb39e9cb92fa21c7436862b05db0c8aab
abdu-n <57192333+abdu-n@users.noreply.github.com>
2020-09-11 09:01:32 +0100

    Create db (#68)
    * added script and step for creating databases
    
    * Add new line
    
    Co-authored-by: abdurrahmannuhu <abdurrahman.nuhu@dwp.gsi.gov.ukgit config --global user.name abdurrahmannuhugit config --global user.email abdurrahman.nuhu@dwp.gsi.gov.uk>

--- bootstrap_actions/cloudwatch.sh
+++ bootstrap_actions/cloudwatch.sh
@@ -166,6 +166,12 @@ cat > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json <<CWAGEN
             "log_group_name": "${cwa_steps_loggrp_name}",
             "log_stream_name": "addresses_tagged.log",
             "timezone": "UTC"
+          },
+          {
+            "file_path": "/var/log/pdm/create_db_sql.log",
+            "log_group_name": "${cwa_steps_loggrp_name}",
+            "log_stream_name": "create_db_sql.log",
+            "timezone": "UTC"
           }
         ]
       }
