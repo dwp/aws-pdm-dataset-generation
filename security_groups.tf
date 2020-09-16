8b1ddc6b0878510ab59a920d3ac9261ad3512856
Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>
2020-09-16 16:44:38 +0100

    S3 too, because searching is hard
    Signed-off-by: Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>

--- security_groups.tf
+++ security_groups.tf
@@ -52,7 +52,7 @@ resource "aws_security_group_rule" "egress_https_s3_endpoint" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  prefix_list_ids   = [data.terraform_remote_state.internal_compute.outputs.vpc.vpc.s3_prefix_list_id]
+  prefix_list_ids   = [data.terraform_remote_state.internal_compute.outputs.vpc.vpc.prefix_list_ids.s3]
   security_group_id = aws_security_group.pdm_common.id
 }
 
@@ -62,7 +62,7 @@ resource "aws_security_group_rule" "egress_http_s3_endpoint" {
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  prefix_list_ids   = [data.terraform_remote_state.internal_compute.outputs.vpc.vpc.s3_prefix_list_id]
+  prefix_list_ids   = [data.terraform_remote_state.internal_compute.outputs.vpc.vpc.prefix_list_ids.s3]
   security_group_id = aws_security_group.pdm_common.id
 }
 
