53365e72acdf10b09e20590f587e526e6d6668d2
camillascuffi <camillascuffi@digital.uc.dwp.gov.uk>
2020-09-28 12:16:15 +0100

    to create database audit

--- steps/create-hive-dynamo-table.sh
+++ steps/create-hive-dynamo-table.sh
@@ -12,10 +12,12 @@ function log_wrapper_message() {
 
 log_wrapper_message "Creating external hive table"
 
-hive -e "CREATE EXTERNAL TABLE IF NOT EXISTS data_pipeline_metadata_hive (Correlation_Id STRING, Run_Id BIGINT, DataProduct STRING, DateProductRun STRING, Status STRING) \
+hive -e "CREATE DATABASE IF NOT EXISTS AUDIT; \
+CREATE EXTERNAL TABLE IF NOT EXISTS AUDIT.data_pipeline_metadata_hive (Correlation_Id STRING, Run_Id BIGINT, DataProduct STRING, DateProductRun STRING, Status STRING) \
 STORED BY 'org.apache.hadoop.hive.dynamodb.DynamoDBStorageHandler' \
-TBLPROPERTIES ('dynamodb.table.name'={dynamodb_table_name}, \
+TBLPROPERTIES ('dynamodb.table.name'='data_pipeline_metadata', \
 'dynamodb.column.mapping' = 'Correlation_Id:Correlation_Id,Run_Id:Run_Id,DataProduct:DataProduct,DateProductRun:Date,Status:Status','dynamodb.null.serialization' = 'true');"
 ) >> /var/log/adg/nohup.log 2>&1
 
 log_wrapper_message "Finished creating external hive table"
+
