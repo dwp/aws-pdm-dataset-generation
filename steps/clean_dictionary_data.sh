e4c2e86f32cb83590e9f0cdd3a5a075a8d51d4cf
abdu-n <57192333+abdu-n@users.noreply.github.com>
2020-09-28 09:47:25 +0100

    Removed spark from PDM (#79)
    * Removed spark from PDM
    
    * Put unclean dictinary data into uclean location
    
    Co-authored-by: abdurrahmannuhu <abdurrahman.nuhu@dwp.gsi.gov.ukgit config --global user.name abdurrahmannuhugit config --global user.email abdurrahman.nuhu@dwp.gsi.gov.uk>

--- steps/clean_dictionary_data.sh
+++ steps/clean_dictionary_data.sh
@@ -14,18 +14,18 @@ DICTIONARY_LOCATION="${dictionary_location}"
     }
 
     log_wrapper_message "Uploading initialOrganisation.json "
-    aws s3 cp $DICTIONARY_LOCATION/site/initialOrganisation.json /opt/emr/sql/initialOrganisation.json
+    aws s3 cp $DICTIONARY_LOCATION/unclean/site/initialOrganisation.json /opt/emr/sql/initialOrganisation.json
     tr -d '\n' < /opt/emr/sql/initialOrganisation.json > /opt/emr/sql/initialOrganisation_tmp.json
     aws s3 cp /opt/emr/sql/initialOrganisation_tmp.json $DICTIONARY_LOCATION/site/initialOrganisation.json
 
     log_wrapper_message "Uploading initialOrganisation-NorthernIreland.json "
-    aws s3 cp $DICTIONARY_LOCATION/site/initialOrganisation-NorthernIreland.json /opt/emr/sql/initialOrganisation-NorthernIreland.json
+    aws s3 cp $DICTIONARY_LOCATION/unclean/site/initialOrganisation-NorthernIreland.json /opt/emr/sql/initialOrganisation-NorthernIreland.json
     tr -d '\n' < /opt/emr/sql/initialOrganisation-NorthernIreland.json > /opt/emr/sql/initialOrganisation-NorthernIreland_tmp.json
     aws s3 cp /opt/emr/sql/initialOrganisation-NorthernIreland_tmp.json $DICTIONARY_LOCATION/site/initialOrganisation-NorthernIreland.json
 
 
     log_wrapper_message "Uploading initialDeliveryUnitAddresses_tagged "
-    aws s3 cp $DICTIONARY_LOCATION/data/address/initialDeliveryUnitAddresses.json /opt/emr/sql/initialDeliveryUnitAddresses.json
+    aws s3 cp $DICTIONARY_LOCATION/unclean/data/address/initialDeliveryUnitAddresses.json /opt/emr/sql/initialDeliveryUnitAddresses.json
     tr -d '\n' < /opt/emr/sql/initialDeliveryUnitAddresses.json > /opt/emr/sql/initialDeliveryUnitAddresses_tmp.json
     ex -sc '1i|{"addresses" : ' -cx /opt/emr/sql/initialDeliveryUnitAddresses_tmp.json
     echo "}" >> /opt/emr/sql/initialDeliveryUnitAddresses_tmp.json
@@ -34,7 +34,7 @@ DICTIONARY_LOCATION="${dictionary_location}"
 
 
     log_wrapper_message "Uploading initialDeliveryUnitAddresses_tagged "
-    aws s3 cp $DICTIONARY_LOCATION/data/address/initialDeliveryUnitAddresses.json /opt/emr/sql/initialDeliveryUnitAddresses.json
+    aws s3 cp $DICTIONARY_LOCATION/unclean/data/address/initialDeliveryUnitAddresses.json /opt/emr/sql/initialDeliveryUnitAddresses.json
     tr -d '\n' < /opt/emr/sql/initialDeliveryUnitAddresses.json > /opt/emr/sql/initialDeliveryUnitAddresses_tmp.json
     aws s3 cp /opt/emr/sql/initialDeliveryUnitAddresses.json $DICTIONARY_LOCATION/data/address/initialDeliveryUnitAddresses.json
 
