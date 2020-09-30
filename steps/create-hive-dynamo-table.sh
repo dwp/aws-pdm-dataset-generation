76036213fbde1b8388e85c9061e617150da2667f
camillascf <camilla.scuffi@dmwgroup.co.uk>
2020-09-30 13:31:15 +0100

    chenged reference name of table

--- steps/create-hive-dynamo-table.sh
+++ steps/create-hive-dynamo-table.sh
@@ -15,7 +15,7 @@ log_wrapper_message "Creating external hive table"
 hive -e "CREATE DATABASE IF NOT EXISTS AUDIT; \
 CREATE EXTERNAL TABLE IF NOT EXISTS AUDIT.data_pipeline_metadata_hive (Correlation_Id STRING, Run_Id BIGINT, DataProduct STRING, DateProductRun STRING, Status STRING) \
 STORED BY 'org.apache.hadoop.hive.dynamodb.DynamoDBStorageHandler' \
-TBLPROPERTIES ('dynamodb.table.name'=${data_pipeline_metadata}, \
+TBLPROPERTIES ('dynamodb.table.name'=${dynamodb_table_name}, \
 'dynamodb.column.mapping' = 'Correlation_Id:Correlation_Id,Run_Id:Run_Id,DataProduct:DataProduct,DateProductRun:Date,Status:Status','dynamodb.null.serialization' = 'true');"
 ) >> /var/log/pdm/create-hive-dynamo-table.log 2>&1
 
