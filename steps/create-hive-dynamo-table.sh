6ca893509b4e7f959d47fc01ab7d8e2f30b9cb05
Levan Kirvalidze <levanik@live.com>
2020-09-30 17:41:53 +0100

    fix log wrapper message

--- steps/create-hive-dynamo-table.sh
+++ steps/create-hive-dynamo-table.sh
@@ -15,10 +15,12 @@ set -euo pipefail
     hive -e "CREATE DATABASE IF NOT EXISTS AUDIT; \
     CREATE EXTERNAL TABLE IF NOT EXISTS AUDIT.data_pipeline_metadata_hive (Correlation_Id STRING, Run_Id BIGINT, DataProduct STRING, DateProductRun STRING, Status STRING) \
     STORED BY 'org.apache.hadoop.hive.dynamodb.DynamoDBStorageHandler' \
-    TBLPROPERTIES ('dynamodb.table.name'='data_pipeline_metadata', \
+    TBLPROPERTIES ('dynamodb.table.name'='${dynamodb_table_name}', \
     'dynamodb.column.mapping' = 'Correlation_Id:Correlation_Id,Run_Id:Run_Id,DataProduct:DataProduct,DateProductRun:Date,Status:Status','dynamodb.null.serialization' = 'true');"
 
     log_wrapper_message "Finished creating external hive table"
+
 ) >> /var/log/pdm/create-hive-dynamo-table.log 2>&1
+
 
 
