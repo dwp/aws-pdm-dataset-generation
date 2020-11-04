258416c334bbb07e473737f28636de74b5e6f2eb
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2020-11-04 12:42:09 +0000

    remove quotes

--- steps/log-tables-row-count.sh
+++ steps/log-tables-row-count.sh
@@ -18,8 +18,8 @@ TRANSACTIONAL_DB="${transactional_db}"
     log_wrapper_message "Running log-tables-row-count.sh file"
     db_names=($VIEWS_DB $MODEL_DB $TRANSFORM_DB $TRANSACTIONAL_DB)
 
-    for db_name in "$${db_names[@]}"; do table_names=$(hive -S -e "use $db_name; show tables;"); declare -a $table_names;
-      for table_name in "$${table_names[@]}"; do row_count=$(hive -S -e "select count(*) from $db_name.$table_name"); echo "$db_name, $table_name, $row_count"
+    for db_name in $${db_names[@]}; do table_names=$(hive -S -e "use $db_name; show tables;"); declare -a $table_names;
+      for table_name in $${table_names[@]}; do row_count=$(hive -S -e "select count(*) from $db_name.$table_name"); echo "$db_name, $table_name, $row_count"
       done
     done
     log_wrapper_message "Ending running log-tables-row-count.sh file"
