25e01a6fe8af782b716291889bbe687d4331b79e
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-02-03 08:11:16 +0000

    Merge

--- local.tf
+++ local.tf
@@ -123,16 +123,11 @@ locals {
   transform_db        = "uc_pdm_transform"
   model_db            = "uc_pdm_model"
   transactional_db    = "uc_pdm_transactional"
-<<<<<<< HEAD
   uc_db               = "uc"
   data_location       = format("s3://%s", data.terraform_remote_state.common.outputs.published_bucket.id)
   dictionary_location = format("s3://%s/%s", data.terraform_remote_state.common.outputs.published_bucket.id, "common-model-inputs")
-=======
   views_db            = "uc_views_tables"
   views_tables_db     = "uc"
-  data_location       = format("s3://%s", data.terraform_remote_state.adg.outputs.published_bucket.id)
-  dictionary_location = format("s3://%s/%s", data.terraform_remote_state.adg.outputs.published_bucket.id, "common-model-inputs")
->>>>>>> b8c8c143033fba892c8825fbec3921d326623e43
   serde               = "org.openx.data.jsonserde.JsonSerDe"
   pii_data_location   = format("s3://%s/%s", data.terraform_remote_state.common.outputs.published_bucket.id, "pdm-dataset/pii")
 
