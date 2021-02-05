2b5d6a185a91f48549d3196995da7869017b37d3
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-02-05 16:57:20 +0000

    Add published bucket to allowed resources for tagging
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- batch_object_tagger.tf
+++ batch_object_tagger.tf
@@ -39,6 +39,7 @@ data "aws_iam_policy_document" "pdm_object_tagger" {
 
     resources = [
       "${data.terraform_remote_state.common.outputs.config_bucket.arn}/*",
+      "${data.terraform_remote_state.common.outputs.published_bucket.arn}/*"
     ]
   }
 }
@@ -84,7 +85,7 @@ resource "aws_batch_job_definition" "pdm_object_tagger" {
       "environment": [
           {"name": "LOG_LEVEL", "value": "INFO"},
           {"name": "AWS_DEFAULT_REGION", "value": "eu-west-2"},
-          {"name": "S3_BUCKET", "value": "${data.terraform_remote_state.common.outputs.config_bucket.id}"},
+          {"name": "BUCKET", "value": "${data.terraform_remote_state.common.outputs.published_bucket.id}"},
           {"name": "ENVIRONMENT", "value": "${local.environment}"}
 
       ],
