81018f202579bdb5d1c48ed3c8c972d8914bc08a
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-02-05 09:46:16 +0000

    Updated batch & pipeline

--- batch_object_tagger.tf
+++ batch_object_tagger.tf
@@ -1,5 +1,5 @@
 locals {
-  pdm_object_tagger_image = "${local.account.management}.${module.vpc.ecr_dkr_domain_name}/dataworks-s3-object-tagger${var.pdm_object_tagger_version}"
+  pdm_object_tagger_image = "${local.account.management}.${data.terraform_remote_state.internal_compute.outputs.vpc.vpc.ecr_dkr_domain_name}/dataworks-s3-object-tagger${var.pdm_object_tagger_version}"
 }
 
 # AWS Batch Job IAM role
@@ -13,7 +13,7 @@ data "aws_iam_policy_document" "batch_assume_policy" {
     ]
 
     principals {
-      identifiers = ["ecs-tasks.amazonaws.com", ]
+      identifiers = ["ecs-tasks.amazonaws.com"]
 
       type = "Service"
     }
@@ -38,14 +38,14 @@ data "aws_iam_policy_document" "pdm_object_tagger" {
     ]
 
     resources = [
-      data.terraform_remote_state.adg.outputs.published_bucket.id,
+      data.terraform_remote_state.adg.outputs.published_bucket.arn,
     ]
   }
 }
 
 resource "aws_iam_policy" "pdm_object_tagger" {
-  name     = "pdm_object_tagger"
-  policy   = data.aws_iam_policy_document.pdm_object_tagger.json
+  name   = "pdm_object_tagger"
+  policy = data.aws_iam_policy_document.pdm_object_tagger.json
 }
 
 resource "aws_iam_role_policy_attachment" "pdm_object_tagger" {
@@ -60,7 +60,7 @@ resource "aws_iam_role_policy_attachment" "pdm_object_tagger_ecs" {
 
 
 resource "aws_batch_job_queue" "pdm_object_tagger" {
-//  TODO: Move compute environment to fargate once Terraform supports it.
+  //  TODO: Move compute environment to fargate once Terraform supports it.
   compute_environments = [data.terraform_remote_state.aws_ingestion.outputs.k2hb_reconciliation_trimmer_batch.arn]
   name                 = "pdm_object_tagger"
   priority             = 10
@@ -68,13 +68,12 @@ resource "aws_batch_job_queue" "pdm_object_tagger" {
 }
 
 resource "aws_batch_job_definition" "pdm_object_tagger" {
-  name       = "pdm_object_tagger_job"
-  type       = "container"
+  name = "pdm_object_tagger_job"
+  type = "container"
 
   container_properties = <<CONTAINER_PROPERTIES
   {
-      "command": ["-m", "Ref::manifest"],
-      "image": "${var.pdm_object_tagger_image}",
+      "image": "${local.pdm_object_tagger_image}",
       "jobRoleArn" : "${aws_iam_role.pdm_object_tagger.arn}",
       "memory": 1024,
       "vcpus": 2,
@@ -82,8 +81,8 @@ resource "aws_batch_job_definition" "pdm_object_tagger" {
           {"name": "LOG_LEVEL", "value": "INFO"},
           {"name": "AWS_DEFAULT_REGION", "value": "eu-west-2"},
           {"name": "S3_BUCKET", "value": "${data.terraform_remote_state.adg.outputs.published_bucket.id}"},
-          {"name": "S3_PREFIX", "value": "pdm-dataset/"},
-          {"name": "CSV_LOCATION", "value": "NOT_SET"}
+          {"name": "ENVIRONMENT", "value": "${local.environment}"}
+
       ],
       "ulimits": [
         {
