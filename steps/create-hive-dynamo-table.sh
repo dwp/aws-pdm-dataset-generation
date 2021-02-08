9a640231e199526af5066943f250f163ed95d45b
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-02-08 14:08:59 +0000

    add new columns
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- steps/create-hive-dynamo-table.sh
+++ steps/create-hive-dynamo-table.sh
@@ -14,10 +14,10 @@ set -euo pipefail
 
     hive -e "CREATE DATABASE IF NOT EXISTS AUDIT; \
     DROP TABLE IF EXISTS AUDIT.data_pipeline_metadata_hive; \
-    CREATE EXTERNAL TABLE IF NOT EXISTS AUDIT.data_pipeline_metadata_hive (Correlation_Id STRING, Run_Id BIGINT, DataProduct STRING, DateProductRun STRING, Status STRING) \
+    CREATE EXTERNAL TABLE IF NOT EXISTS AUDIT.data_pipeline_metadata_hive (Correlation_Id STRING, Run_Id BIGINT, DataProduct STRING, DateProductRun STRING, Status STRING, CurrentStep STRING) \
     STORED BY 'org.apache.hadoop.hive.dynamodb.DynamoDBStorageHandler' \
     TBLPROPERTIES ('dynamodb.table.name'='${dynamodb_table_name}', \
-    'dynamodb.column.mapping' = 'Correlation_Id:Correlation_Id,Run_Id:Run_Id,DataProduct:DataProduct,DateProductRun:Date,Status:Status','dynamodb.null.serialization' = 'true');"
+    'dynamodb.column.mapping' = 'Correlation_Id:Correlation_Id,Run_Id:Run_Id,DataProduct:DataProduct,DateProductRun:Date,Status:Status,CurrentStep:CurrentStep','dynamodb.null.serialization' = 'true');"
 
     log_wrapper_message "Finished creating external hive table"
 
