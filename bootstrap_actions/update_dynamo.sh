eba62eefd9835d68ae01602075d18707cd43cd64
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-02-15 18:44:40 +0000

    use functions to allow recursive calls
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- bootstrap_actions/update_dynamo.sh
+++ bootstrap_actions/update_dynamo.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 (
+    INSTANCE_ROLE=$(jq .instanceRole /mnt/var/lib/info/extraInstanceData.json)
+
+    #only log on master to avoid duplication
+    if [[ "$INSTANCE_ROLE" != '"MASTER"' ]]; then
+      exit
+    fi
     source /opt/emr/logging.sh
 
     function log_wrapper_message() {
         log_pdm_message "$${1}" "update_dynamo.sh" "$${PID}" "$${@:2}" "Running as: ,$USER"
     }
 
-  log_wrapper_message "Start running update_dynamo.s Shell"
+  log_wrapper_message "Start running update_dynamo.sh Shell"
 
   STEP_DETAILS_DIR=/mnt/var/lib/info/steps
   CORRELATION_ID_FILE=/opt/emr/correlation_id.txt
@@ -38,48 +44,60 @@
   JSON_STRING=`jq '.Run_Id.N = "'$RUN_ID'"'<<<$JSON_STRING`
   JSON_STRING=`jq '.Status.S = "'$STATUS'"'<<<$JSON_STRING`
 
-  #Check if row for this correlation ID already exists - in which case we need to increment the Run_Id
-  response=`aws dynamodb get-item --table-name ${dynamodb_table_name} --key '{"Correlation_Id": {"S": "'$CORRELATION_ID'"}, "DataProduct": {"S": "'$DATA_PRODUCT'"}}'`
-  if [[ -z $response ]]; then
-    aws dynamodb put-item  --table-name ${dynamodb_table_name} --item "$JSON_STRING"
-  else
-    RUN_ID=`echo $response | jq -r .'Item.Run_Id.N'`
-    RUN_ID=$((RUN_ID+1))
-    JSON_STRING=`jq '.Run_Id.N = "'$RUN_ID'"'<<<$JSON_STRING`
-    aws dynamodb put-item  --table-name ${dynamodb_table_name} --item "$JSON_STRING"
-  fi
-  log_wrapper_message "Updating DynamoDB with CORRELATION_ID: $CORRELATION_ID and RUN_ID: $RUN_ID"
 
-  keep_looking=true
-  PREVIOUS_STEP=""
-  PREVIOUS_STATE=""
-  #endless loop to keep updating dynamo every 30s with current step info
-  cd $STEP_DETAILS_DIR
-  while [ $keep_looking ]; do
+  processed_files=()
+  dynamo_put_item() {
+    JSON_STRING=$1
+    aws dynamodb put-item  --table-name ${dynamodb_table_name} --item "$JSON_STRING"
+  }
+  check_step_dir() {
+    cd $STEP_DETAILS_DIR
     for i in $STEP_DETAILS_DIR/*.json; do
-      state=""
+      if [[ "$${processed_files[@]}" =~ "$${i}" ]]; then
+        continue
+      fi
+      state=$(jq -r '.state' $i)
       while [[ "$state" != "COMPLETED" ]]; do
         step_script_name=$(jq -r '.args[0]' $i)
         CURRENT_STEP=$(echo "$step_script_name" | sed 's:.*/::' | cut -f 1 -d '.')
-        state=$(jq -r '.state' $i)
         JSON_STRING=`jq '.CurrentStep.S = "'$CURRENT_STEP'"'<<<$JSON_STRING`
+        state=$(jq -r '.state' $i)
         if [[ "$state" == "FAILED" ]]; then
+          log_wrapper_message "Failed. Step Name: $step_script_name, Step status: $state"
           JSON_STRING=`jq '.Status.S = "'$state'"'<<<$JSON_STRING`
         fi
         if [[ "$step_script_name" == "$FINAL_STEP_NAME" ]] && [[ "$state" == "COMPLETED" ]]; then
           JSON_STRING=`jq '.Status.S = "'$state'"'<<<$JSON_STRING`
-          keep_looking=false
         fi
         if [[ $PREVIOUS_STATE != $state ]] && [[ $PREVIOUS_STEP != $CURRENT_STEP ]]; then
-          aws dynamodb put-item  --table-name ${dynamodb_table_name} --item "$JSON_STRING"
-          log_wrapper_message "Updating DynamoDB with step: $CURRENT_STEP"
+          #aws dynamodb put-item  --table-name ${dynamodb_table_name} --item "$JSON_STRING"
+          dynamo_put_item "$JSON_STRING"
+          log_wrapper_message "Success. Step Name: $step_script_name, Step status: $state"
+          processed_files+=( $i )
         else
+          log_wrapper_message "Sleeping as $step_script_name is still in state: $state"
           sleep 30
         fi
         PREVIOUS_STATE=$state
         PREVIOUS_STEP=$CURRENT_STEP
       done
     done
-  done
+    check_step_dir
+  }
+
+  #Check if row for this correlation ID already exists - in which case we need to increment the Run_Id
+  response=`aws dynamodb get-item --table-name ${dynamodb_table_name} --key '{"Correlation_Id": {"S": "'$CORRELATION_ID'"}, "DataProduct": {"S": "'$DATA_PRODUCT'"}}'`
+  if [[ -z $response ]]; then
+    dynamo_put_item "$JSON_STRING"
+  else
+    RUN_ID=`echo $response | jq -r .'Item.Run_Id.N'`
+    RUN_ID=$((RUN_ID+1))
+    JSON_STRING=`jq '.Run_Id.N = "'$RUN_ID'"'<<<$JSON_STRING`
+    dynamo_put_item "$JSON_STRING"
+  fi
+  log_wrapper_message "Updating DynamoDB with CORRELATION_ID: $CORRELATION_ID and RUN_ID: $RUN_ID"
+
+  #kick off loop to process all step files
+  check_step_dir
 
 ) >> /var/log/pdm/update_dynamo_sh.log 2>&1
