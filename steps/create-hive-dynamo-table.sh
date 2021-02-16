a4edaffb5e33de9a4e56e23c14a01b1f78475093
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-02-16 17:55:58 +0000

    fmt
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- steps/create-hive-dynamo-table.sh
+++ steps/create-hive-dynamo-table.sh
@@ -3,7 +3,7 @@
 
 (
     ########################################################################################
-    dbs=( "uc_pdm_source" "uc_pdm_transactional" "uc_pdm_transform" "uc_pdm_model" "uc_views_tables" "uc")
+    dbs=("uc_pdm_source" "uc_pdm_transactional" "uc_pdm_transform" "uc_pdm_model" "uc_views_tables" "uc")
     declare -a $dbs
     for db in ${dbs[@]}; do
       hive -e "DROP DATABASE $db cascade"
