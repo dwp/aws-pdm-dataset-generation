d07fe6fed341c54312d789c4dffe7cdf3eacc8b2
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-02-16 19:24:56 +0000

    add pipefail and remove drop script
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- utils/delete_dbs_tables.sh
+++ utils/delete_dbs_tables.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-    dbs=( "uc_pdm_source" "uc_pdm_transactional" "uc_pdm_transform" "uc_pdm_model" "uc")
+    dbs=("uc_pdm_source" "uc_pdm_transactional" "uc_pdm_transform" "uc_pdm_model" "uc_views_tables" "uc")
     declare -a $dbs
-    for db in ${dbs[@]}; do
+    for db in $${dbs[@]}; do
       hive -e "DROP DATABASE $db cascade"
     done
