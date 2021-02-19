72d7667e6b81712d2f2408322a526849da416507
Mark Stevens <mark.stevens@infinityworks.com>
2021-02-19 14:07:12 +0000

    removed additional dollar from bash variable (#179)
    Signed-off-by: Mark <mark.stevens@infinityworks.com>

--- bootstrap_actions/resume_step.sh
+++ bootstrap_actions/resume_step.sh
@@ -9,7 +9,8 @@ function resume_from_step() {
         log_pdm_message "Previous step file found"
         STEP=`cat $STEP_TO_START_FROM_FILE`
         CURRENT_FILE_NAME=`basename "$0"`
-        FILE_NAME_NO_EXT="$${CURRENT_FILE_NAME%.*}"
+        FILE_NAME_NO_EXT="${CURRENT_FILE_NAME%.*}"
+        log_pdm_message "Current file name $FILE_NAME_NO_EXT"
 
         if [[ $STEP != $FILE_NAME_NO_EXT ]]; then
             log_pdm_message "Current step name $FILE_NAME_NO_EXT doesn't match previously failed step $STEP, exiting"
