f685f05eaf8e79c08c0359fd8d6f340e2424ee81
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-02-22 08:56:07 +0000

    New script

---
+++ bootstrap_actions/download_scripts.sh
@@ -0,0 +1,26 @@
+(
+    # Import the logging functions
+    source /opt/emr/logging.sh
+
+    function log_wrapper_message() {
+        log_pdm_message "$${1}" "download_scripts.sh" "$${PID}" "$${@:2}" "Running as: ,$USER"
+    }
+
+    echo "Download & install latest bootstrap and steps scripts"
+    log_wrapper_message "Downloading & install latest bootstrap and steps scripts"
+
+    aws s3 cp --recursive ${scripts_location}/ /var/ci/ --include "*.sh"
+
+    echo "Download & install latest metrics scripts"
+    log_wrapper_message "Downloading & install latest metrics scripts"
+
+    aws s3 cp --recursive ${metrics_scripts_location}/ /var/ci/ --include "*.sh"
+
+    echo "Apply recursive execute permissions to the folder"
+    log_wrapper_message "Apply recursive execute permissions to the folder"
+
+    sudo chmod --recursive a+rx /var/ci
+
+    echo "Script downloads completed"
+    log_wrapper_message "Script downloads completed"
+)  >> /var/log/pdm/download_scripts.log 2>&1
