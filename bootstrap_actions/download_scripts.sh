cedf4f05db08ffec80c27123bc524ede485fe1f3
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-02-22 13:13:02 +0000

    Create log folder

--- bootstrap_actions/download_scripts.sh
+++ bootstrap_actions/download_scripts.sh
@@ -1,3 +1,7 @@
+# Necessary minimal setup for this script to run
+sudo mkdir -p /var/log/pdm
+sudo chown hadoop:hadoop /var/log/pdm
+
 (
     # Import the logging functions
     source /opt/emr/logging.sh
@@ -6,21 +10,19 @@
         log_pdm_message "$${1}" "download_scripts.sh" "$${PID}" "$${@:2}" "Running as: ,$USER"
     }
 
-    echo "Download & install latest bootstrap and steps scripts"
-    log_wrapper_message "Downloading & install latest bootstrap and steps scripts"
+    log_wrapper_message "Create directory for the files"
+    sudo mkdir -p /var/ci
+    sudo chown hadoop:hadoop /var/ci
 
+    log_wrapper_message "Downloading & install latest bootstrap and steps scripts"
     $(which aws) s3 cp ${scripts_location}/ /var/ci/ --include "*.sh"
 
-    echo "Download & install latest metrics scripts"
     log_wrapper_message "Downloading & install latest metrics scripts"
-
     $(which aws) s3 cp ${metrics_scripts_location}/ /var/ci/ --include "*.sh"
 
-    echo "Apply recursive execute permissions to the folder"
     log_wrapper_message "Apply recursive execute permissions to the folder"
-
     sudo chmod --recursive a+rx /var/ci
 
-    echo "Script downloads completed"
     log_wrapper_message "Script downloads completed"
+
 )  >> /var/log/pdm/download_scripts.log 2>&1
