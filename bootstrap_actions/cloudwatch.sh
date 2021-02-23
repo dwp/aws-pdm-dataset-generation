aaafd6079a7ba64a8bfdbb3615b847a9b56005ca
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-02-23 14:23:49 +0000

    move log to bootstrap section as its a bootstrap action
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- bootstrap_actions/cloudwatch.sh
+++ bootstrap_actions/cloudwatch.sh
@@ -81,6 +81,12 @@ cat > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json <<CWAGEN
             "timezone": "UTC"
           },
           {
+            "file_path": "/var/log/pdm/download_sql.log",
+            "log_group_name": "$${cwa_bootstrap_loggrp_name}",
+            "log_stream_name": "{instance_id}-download_sql.log",
+            "timezone": "UTC"
+          },
+          {
             "file_path": "/var/log/pdm/application_metrics.log",
             "log_group_name": "$${cwa_bootstrap_loggrp_name}",
             "log_stream_name": "{instance_id}-application_metrics.log",
@@ -120,12 +126,6 @@ cat > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json <<CWAGEN
             "file_path": "/var/log/pdm/download_unzip_sql.log",
             "log_group_name": "$${cwa_steps_loggrp_name}",
             "log_stream_name": "{instance_id}-download_unzip_sql.log",
-            "timezone": "UTC"
-          },
-          {
-            "file_path": "/var/log/pdm/download_sql.log",
-            "log_group_name": "$${cwa_steps_loggrp_name}",
-            "log_stream_name": "{instance_id}-download_sql.log",
             "timezone": "UTC"
           },
           {
