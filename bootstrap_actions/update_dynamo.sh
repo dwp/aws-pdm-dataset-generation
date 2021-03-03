041e87f99f12014a0cccd027f66c77991a5b0a71
steveburton4 <Steven.burton@hotmail.co.uk>
2021-03-03 11:53:08 +0000

    Use update item instead of put and add ttl (#206)
    * Use update item instead of put and add ttl
    
    * Add log and fix comma

--- bootstrap_actions/update_dynamo.sh
+++ bootstrap_actions/update_dynamo.sh
@@ -19,8 +19,6 @@
   S3_PREFIX_FILE=/opt/emr/s3_prefix.txt
   RUN_ID=1
   DATE=$(date '+%Y-%m-%d')
-  STATUS="In-Progress"
-  CURRENT_STEP=""
   DATA_PRODUCT="PDM"
   CLUSTER_ID=`cat /mnt/var/lib/info/job-flow.json | jq '.jobFlowId'`
   CLUSTER_ID=$${CLUSTER_ID//\"}
@@ -42,19 +40,49 @@
     sleep 5
   done
 
-  JSON_STRING=`cat /opt/emr/dynamo_schema.json`
-  JSON_STRING=`jq '.Correlation_Id.S = "'$CORRELATION_ID'"'<<<$JSON_STRING`
-  JSON_STRING=`jq '.Date.S = "'$DATE'"'<<<$JSON_STRING`
-  JSON_STRING=`jq '.Run_Id.N = "'$RUN_ID'"'<<<$JSON_STRING`
-  JSON_STRING=`jq '.Status.S = "'$STATUS'"'<<<$JSON_STRING`
-  JSON_STRING=`jq '.Cluster_Id.S = "'$CLUSTER_ID'"'<<<$JSON_STRING`
-  JSON_STRING=`jq '.S3_Prefix_Analytical_DataSet.S = "'$S3_PREFIX'"'<<<$JSON_STRING`
-
   processed_files=()
-  dynamo_put_item() {
-    JSON_STRING=$1
-    aws dynamodb put-item  --table-name ${dynamodb_table_name} --item "$JSON_STRING"
+
+  get_ttl() {
+      TIME_NOW=$(($(date +'%s * 1000 + %-N / 1000000')))
+      echo $((TIME_NOW + 604800000))
   }
+
+  dynamo_put_item() {
+    current_step="$1"
+    status="$2"
+    run_id="$3"
+
+    ttl_value=$(get_ttl)
+
+    log_wrapper_message "Updating DynamoDB with Correlation_Id: $CORRELATION_ID, DataProduct: $DATA_PRODUCT, Date: $DATE, Cluster_Id: $CLUSTER_ID, S3_Prefix_Analytical_DataSet: $S3_PREFIX, TimeToExist: $ttl_value, CurrentStep: $current_step, Status: $status, Run_Id: $run_id"
+
+    update_expression="SET #d = :s, Cluster_Id = :v, S3_Prefix_Analytical_DataSet = :b, TimeToExist = :z"
+    expression_values="\":s\": {\"S\":\"$DATE\"}, \":v\": {\"S\":\"$CLUSTER_ID\"}, \":b\": {\"S\":\"$S3_PREFIX\"}, \":z\": {\"N\":\"$ttl_value\"}"
+    expression_names="\"#d\":\"Date\""
+
+    if [[ ! -z "$current_step" ]] && [[ "$current_step" != "NOT_SET" ]]; then
+        update_expression="$update_expression, CurrentStep = :y"
+        expression_values="$expression_values, \":y\": {\"S\":\"$current_step\"}"
+    fi
+
+    if [[ ! -z "$status" ]] && [[ "$status" != "NOT_SET" ]]; then
+        update_expression="$update_expression, #a = :u"
+        expression_values="$expression_values, \":u\": {\"S\":\"$status\"}"
+        expression_names="$expression_names, \"#a\":\"Status\""
+    fi
+
+    if [[ ! -z "$run_id" ]] && [[ "$run_id" != "NOT_SET" ]]; then
+        update_expression="$update_expression, Run_Id = :t"
+        expression_values="$expression_values, \":t\": {\"N\":\"$run_id\"}"
+    fi
+
+    $(which aws) dynamodb update-item  --table-name "${dynamodb_table_name}" \
+        --key "{\"Correlation_Id\":{\"S\":\"$CORRELATION_ID\"},\"DataProduct\":{\"S\":\"$DATA_PRODUCT\"}}" \
+        --update-expression "$update_expression" \
+        --expression-attribute-values "{$expression_values}" \
+        --expression-attribute-names "{$expression_names}"
+  }
+
   check_step_dir() {
     cd $STEP_DETAILS_DIR
     for i in $STEP_DETAILS_DIR/*.json; do
@@ -65,22 +93,18 @@
       while [[ "$state" != "COMPLETED" ]]; do
         step_script_name=$(jq -r '.args[0]' $i)
         CURRENT_STEP=$(echo "$step_script_name" | sed 's:.*/::' | cut -f 1 -d '.')
-        JSON_STRING=`jq '.CurrentStep.S = "'$CURRENT_STEP'"'<<<$JSON_STRING`
         state=$(jq -r '.state' $i)
         if [[ "$state" == "FAILED" ]] || [[ "$state" == "CANCELLED" ]]; then
           log_wrapper_message "Failed. Step Name: $CURRENT_STEP, Step status: $state"
-          JSON_STRING=`jq '.Status.S = "FAILED"'<<<$JSON_STRING`
-          dynamo_put_item "$JSON_STRING"
+          dynamo_update_item "$CURRENT_STEP" "FAILED" "NOT_SET"
           exit 0
         fi
         if [[ "$CURRENT_STEP" == "$FINAL_STEP_NAME" ]] && [[ "$state" == "COMPLETED" ]]; then
-          JSON_STRING=`jq '.Status.S = "COMPLETED"'<<<$JSON_STRING`
-          dynamo_put_item "$JSON_STRING"
+          dynamo_update_item "$CURRENT_STEP" "COMPLETED" "NOT_SET"
           exit 0
         fi
         if [[ $PREVIOUS_STATE != $state ]] && [[ $PREVIOUS_STEP != $CURRENT_STEP ]]; then
-          
-          dynamo_put_item "$JSON_STRING"
+          dynamo_update_item "$CURRENT_STEP" "COMPLETED" "NOT_SET"
           log_wrapper_message "Success. Step Name: $CURRENT_STEP, Step status: $state"
           processed_files+=( $i )
         else
@@ -90,29 +114,28 @@
         PREVIOUS_STEP=$CURRENT_STEP
       done
     done
+    sleep 5
     check_step_dir
   }
 
   #Check if row for this correlation ID already exists - in which case we need to increment the Run_Id
   response=`aws dynamodb get-item --table-name ${dynamodb_table_name} --key '{"Correlation_Id": {"S": "'$CORRELATION_ID'"}, "DataProduct": {"S": "'$DATA_PRODUCT'"}}'`
   if [[ -z $response ]]; then
-    dynamo_put_item "$JSON_STRING"
+    dynamo_update_item "NOT_SET" "In-Progress" "1"
   else
-    STATUS=`echo $response | jq -r .'Item.Status.S'`
-    log_wrapper_message "Status from previous run $STATUS"
-    if [[ "$STATUS" == "FAILED" ]]; then
+    LAST_STATUS=`echo $response | jq -r .'Item.Status.S'`
+    log_wrapper_message "Status from previous run $LAST_STATUS"
+    if [[ "$LAST_STATUS" == "FAILED" ]]; then
       log_wrapper_message "Previous failed status found, creating step_to_start_from.txt"
       CURRENT_STEP=`echo $response | jq -r .'Item.CurrentStep.S'`
       echo $CURRENT_STEP >> /opt/emr/step_to_start_from.txt
     fi   
 
-    RUN_ID=`echo $response | jq -r .'Item.Run_Id.N'`
-    RUN_ID=$((RUN_ID+1))
-    JSON_STRING=`jq '.Run_Id.N = "'$RUN_ID'"'<<<$JSON_STRING`
-    JSON_STRING=`jq '.Date.S = "'$DATE'"'<<<$JSON_STRING`
-    dynamo_put_item "$JSON_STRING"
+    CURRENT_RUN_ID=`echo $response | jq -r .'Item.Run_Id.N'`
+    NEW_RUN_ID=$((CURRENT_RUN_ID+1))
+    dynamo_update_item "NOT_SET" "In-Progress" "$NEW_RUN_ID"
   fi
-  log_wrapper_message "Updating DynamoDB with CORRELATION_ID: $CORRELATION_ID and RUN_ID: $RUN_ID"
+  log_wrapper_message "Updating DynamoDB with CORRELATION_ID: $CORRELATION_ID and RUN_ID: $NEW_RUN_ID"
 
   #kick off loop to process all step files
   check_step_dir
