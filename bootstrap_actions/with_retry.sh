ebc73b69dec80dba35f9e9cd1e7d803a5c68746d
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-03-04 13:32:40 +0000

    add new log file that logs time taken for each sql
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- bootstrap_actions/with_retry.sh
+++ bootstrap_actions/with_retry.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/bash
 
-source /opt/emr/retry.sh
+(
 
-retry::with_retries $@
+    source /opt/emr/retry.sh
+    source /opt/emr/logging.sh
+
+    function log_wrapper_message() {
+        log_pdm_message "$${1}" "update_dynamo.sh" "$${PID}" "$${@:2}" "Running as: ,$USER"
+    }
+
+    SECONDS=0
+    retry::with_retries $@
+    DURATION=$SECONDS
+
+    SCRIPT_NAME=`echo $@ | sed 's/.*scripts//'`
+    log_wrapper_message "$SCRIPT_NAME took $SECONDS seconds to process"
+
+)  >> /var/log/pdm/time_taken.log 2>&1
