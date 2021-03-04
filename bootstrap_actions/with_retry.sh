88e1eea2a9b9a83efa7d8aacae9f47f1a5d1f4cc
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-03-04 14:16:13 +0000

    better logs
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- bootstrap_actions/with_retry.sh
+++ bootstrap_actions/with_retry.sh
@@ -6,8 +6,10 @@
     source /opt/emr/logging.sh
 
     function log_wrapper_message() {
-        log_pdm_message "$${1}" "update_dynamo.sh" "$${PID}" "$${@:2}" "Running as: ,$USER"
+        log_pdm_message "$${1}" "with_retry.sh" "$${PID}" "$${@:2}" "Running as: ,$USER"
     }
+
+    log_wrapper_message "Starting with_retry.sh script................"
 
     SECONDS=0
     retry::with_retries $@
