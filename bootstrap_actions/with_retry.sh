79a5b5fcafd1a58e513b5311f783b52dc686bbe5
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-03-04 15:00:55 +0000

    put logs to normal
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- bootstrap_actions/with_retry.sh
+++ bootstrap_actions/with_retry.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/bash
 
-(
+source /opt/emr/retry.sh
 
-    source /opt/emr/retry.sh
-    source /opt/emr/logging.sh
+SECONDS=0
+retry::with_retries $@
+DURATION=$SECONDS
 
-    function log_wrapper_message() {
-        log_pdm_message "$${1}" "with_retry.sh" "$${PID}" "$${@:2}" "Running as: ,$USER"
-    }
+SCRIPT_NAME=`echo $@ | sed 's/.*scripts//'`
+log_wrapper_message "$SCRIPT_NAME took $SECONDS seconds to process"
 
-    log_wrapper_message "Starting with_retry.sh script................"
-
-    SECONDS=0
-    retry::with_retries $@
-    DURATION=$SECONDS
-
-    SCRIPT_NAME=`echo $@ | sed 's/.*scripts//'`
-    log_wrapper_message "$SCRIPT_NAME took $SECONDS seconds to process"
-
-)  >> /var/log/pdm/time_taken.log 2>&1
