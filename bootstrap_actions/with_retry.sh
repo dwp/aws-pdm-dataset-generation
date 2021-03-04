cb977f8c75c3daa4b429c65286d584ab9bf33da9
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-03-04 16:40:36 +0000

    log
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- bootstrap_actions/with_retry.sh
+++ bootstrap_actions/with_retry.sh
@@ -4,13 +4,17 @@
 source /opt/emr/retry.sh
 source /opt/emr/logging.sh
 
+SCRIPT_NAME=`echo $@ | sed 's/.*scripts//'`
+
 function log_wrapper_message() {
     log_pdm_message "$${1}" "with_retry.sh" "$${PID}" "$${@:2}" "Running as: ,$USER"
 }
+
+
+log_wrapper_message "Starting processing $SCRIPT_NAME ............................"
 
 SECONDS=0
 retry::with_retries $@
 DURATION=$SECONDS
 
-SCRIPT_NAME=`echo $@ | sed 's/.*scripts//'`
 log_wrapper_message "$SCRIPT_NAME took $SECONDS seconds to process"
