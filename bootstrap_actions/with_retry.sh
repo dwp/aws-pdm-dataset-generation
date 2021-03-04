4f9a78fcdda8d469c7e717bd8bbac8b46eacd7c6
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2021-03-04 11:50:25 +0000

    log times taken for each script
    Signed-off-by: levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>

--- bootstrap_actions/with_retry.sh
+++ bootstrap_actions/with_retry.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/bash
 
 source /opt/emr/retry.sh
+source /opt/emr/logging.sh
 
-retry::with_retries $@
+function log_wrapper_message() {
+    log_pdm_message "$${1}" "update_dynamo.sh" "$${PID}" "$${@:2}" "Running as: ,$USER"
+}
+
+
+
+SECONDS=0
+time retry::with_retries $@
+DURATION=$SECONDS
+echo time1-time2 $@
+
+log_wrapper_message "$@ took $SECONDS seconds to process"
