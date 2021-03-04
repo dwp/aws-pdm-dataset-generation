48c18bbaf1d73653f3d62408b2ee8949fa35291d
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-03-04 08:54:09 +0000

    LLAP

--- local.tf
+++ local.tf
@@ -214,7 +214,7 @@ locals {
 
   transform_processes = {
     development = "20"
-    qa          = "20"
+    qa          = "1"
     integration = "10"
     preprod     = "10"
     production  = "20"
