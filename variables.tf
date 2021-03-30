ce07dcc983a6633cc20166c51e5681fbea698795
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-03-30 09:56:23 +0100

    Bump up dev and qa

--- variables.tf
+++ variables.tf
@@ -49,8 +49,8 @@ variable "emr_instance_type_master" {
 
 variable "emr_instance_type_core_one" {
   default = {
-    development = "r5.4xlarge"
-    qa          = "r5.4xlarge"
+    development = "r5.12xlarge"
+    qa          = "r5.12xlarge"
     integration = "r5.4xlarge"
     preprod     = "r5.4xlarge"
     production  = "r5.12xlarge"
@@ -69,8 +69,8 @@ variable "emr_instance_type_weighting_core_one" {
 
 variable "emr_instance_type_core_two" {
   default = {
-    development = "r5a.4xlarge"
-    qa          = "r5a.4xlarge"
+    development = "r5a.12xlarge"
+    qa          = "r5a.12xlarge"
     integration = "r5a.4xlarge"
     preprod     = "r5a.4xlarge"
     production  = "r5a.12xlarge"
@@ -89,8 +89,8 @@ variable "emr_instance_type_weighting_core_two" {
 
 variable "emr_instance_type_core_three" {
   default = {
-    development = "r5d.4xlarge"
-    qa          = "r5d.4xlarge"
+    development = "r5d.12xlarge"
+    qa          = "r5d.12xlarge"
     integration = "r5d.4xlarge"
     preprod     = "r5d.4xlarge"
     production  = "r5d.12xlarge"
@@ -109,8 +109,8 @@ variable "emr_instance_type_weighting_core_three" {
 
 variable "emr_instance_type_core_four" {
   default = {
-    development = "m5.8xlarge"
-    qa          = "m5.8xlarge"
+    development = "m5.24xlarge"
+    qa          = "m5.24xlarge"
     integration = "m5.8xlarge"
     preprod     = "m5.8xlarge"
     production  = "m5.24xlarge"
@@ -129,10 +129,10 @@ variable "emr_instance_type_weighting_core_four" {
 
 variable "emr_instance_type_core_five" {
   default = {
-    development = "i3en.3xlarge"
-    qa          = "i3en.3xlarge"
-    integration = "i3en.3xlarge"
-    preprod     = "i3en.3xlarge"
+    development = "i3en.12xlarge"
+    qa          = "i3en.12xlarge"
+    integration = "i3en.6xlarge"
+    preprod     = "i3en.6xlarge"
     production  = "i3en.12xlarge"
   }
 }
@@ -155,8 +155,8 @@ variable "metadata_store_pdm_writer_username" {
 # This is weighted not a count of instances
 variable "emr_core_instance_capacity_on_demand" {
   default = {
-    development = "5"
-    qa          = "5"
+    development = "25"
+    qa          = "25"
     integration = "5"
     preprod     = "5"
     production  = "25"
@@ -166,8 +166,8 @@ variable "emr_core_instance_capacity_on_demand" {
 # This is weighted not a count of instances
 variable "emr_core_instance_capacity_spot" {
   default = {
-    development = "45"
-    qa          = "45"
+    development = "150"
+    qa          = "150"
     integration = "45"
     preprod     = "45"
     production  = "150"
@@ -192,6 +192,6 @@ variable "emr_spot_timeout_duration_minutes" {
     qa          = "5"
     integration = "5"
     preprod     = "5"
-    production  = "20"
+    production  = "5"
   }
 }
