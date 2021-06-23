ab204cd61f254adfea0c64556daf4b9dae473ab8
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-06-23 15:52:00 +0100

    Use 2a

--- local.tf
+++ local.tf
@@ -408,7 +408,7 @@ locals {
     production  = true
   }
 
-  emr_subnet_non_capacity_reserved_environments = "eu-west-2c"
+  emr_subnet_non_capacity_reserved_environments = "eu-west-2a"
 
   data_classification = {
     config_bucket  = data.terraform_remote_state.common.outputs.config_bucket
