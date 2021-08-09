ee95d9f8478219822104fc0b062ee4372a5499ce
levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>
2021-08-09 16:56:55 +0100

    linting
    Signed-off-by: levankirvalidze <levankirvalidze@digital.uc.dwp.gov.uk>

--- get_lambda_release.sh
+++ get_lambda_release.sh
@@ -6,8 +6,8 @@ get_release_information(){
 }
 
 update_tfvars() {
-    tfvars_line=$(echo "$(echo ${REPO} | sed 's/-/_/g')_$extension = { base_path = \"../${REPO}-release\", version = \"${VERSION}\" }")
-    if ! cat terraform.tfvars | grep -q "$tfvars_line"; then
+    tfvars_line=$(echo "$(echo "${REPO}" | sed 's/-/_/g')_$extension = { base_path = \"../${REPO}-release\", version = \"${VERSION}\" }")
+    if ! grep -q "$tfvars_line" terraform.tfvars ; then
         echo "$tfvars_line" >> terraform.tfvars
     fi
 }
@@ -28,7 +28,7 @@ get_release() {
 }
 
 fetch_asset(){
-    EXISTING_VERSION=$(ls ../${REPO}-release/ | grep "${VERSION}")
+    EXISTING_VERSION=$(ls ../"${REPO}"-release/ | grep "${VERSION}")
 
     if [[ -f ../${REPO}-release/$EXISTING_VERSION ]]; then
         echo "${REPO}-release/$EXISTING_VERSION already exists: Skipping download"
