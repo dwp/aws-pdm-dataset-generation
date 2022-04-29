1b7a00b8384a7eeff4972bf69a2a3ebd52fa8e02
Pjay <38280665+gbpjay@users.noreply.github.com>
2022-04-29 14:56:56 +0100

    Update local.tf (#396)
    Testing care leaver code in lower env

--- local.tf
+++ local.tf
@@ -81,10 +81,10 @@ locals {
   }
 
   pdm_version = {
-    development = "0.0.70"
-    qa          = "0.0.70"
-    integration = "0.0.70"
-    preprod     = "0.0.70"
+    development = "0.0.71"
+    qa          = "0.0.71"
+    integration = "0.0.71"
+    preprod     = "0.0.71"
     production  = "0.0.70"
   }
 
