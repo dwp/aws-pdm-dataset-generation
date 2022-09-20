94bf3bac494acefa8747eb69c3a51d14c79dd013
Dave Siddons <davesiddons@siddo.plus.com>
2022-09-20 10:49:52 +0100

    Dripfeedconfigchange (#413)
    * first few changes
    
    * terraform fmt
    
    * drip feeding
    
    * nexttweaks
    
    * changed instance type
    
    * change instance type
    
    Co-authored-by: Dave Siddons <david.siddons@engineeering.digital.dwp.gov.uk>

--- variables.tf
+++ variables.tf
@@ -52,7 +52,7 @@ variable "emr_instance_type_core_one" {
     development = "m5.2xlarge"
     qa          = "m5.2xlarge"
     integration = "m5.2xlarge"
-    preprod     = "m5.16xlarge"
+    preprod     = "m5.8xlarge"
     production  = "m5.16xlarge"
   }
 }
