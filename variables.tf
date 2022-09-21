9e8a89f4de120206f5ff0df0aef29f96a55f9e5e
Dave Siddons <david.siddons@engineeering.digital.dwp.gov.uk>
2022-09-21 16:47:33 +0100

    lets try just the hive config changes to see how perf affected

--- variables.tf
+++ variables.tf
@@ -52,7 +52,7 @@ variable "emr_instance_type_core_one" {
     development = "m5.2xlarge"
     qa          = "m5.2xlarge"
     integration = "m5.2xlarge"
-    preprod     = "m5.8xlarge"
+    preprod     = "m5.16xlarge"
     production  = "m5.16xlarge"
   }
 }
@@ -68,7 +68,7 @@ variable "emr_core_instance_capacity_on_demand" {
     development = "10"
     qa          = "10"
     integration = "10"
-    preprod     = "120"
+    preprod     = "39"
     production  = "39"
   }
 }
