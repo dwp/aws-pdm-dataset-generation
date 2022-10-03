e90bfc120112830ebb43be13d7be1e8363606154
rorybryett <rory.bryett@dwp.gov.uk>
2022-10-03 15:59:21 +0100

    upgrade terraform 1.0.11

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,7 @@ output "private_dns" {
     pdm_service_discovery_dns = aws_service_discovery_private_dns_namespace.pdm_services
     pdm_service_discovery     = aws_service_discovery_service.pdm_services
   }
+  sensitive = true
 }
 
 output "pdm_emr_launcher_lambda" {
