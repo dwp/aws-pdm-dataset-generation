99bb123b85776be467b894855779efa4e94ceb2d
joe-edmond <89150867+joe-edmond@users.noreply.github.com>
2022-11-23 14:52:02 +0000

    Bumped PDM version in PreProd (#435)
    Co-authored-by: joeedmond <joe.Edmond@engineering.digital.dwp.gov.uk>

--- local.tf
+++ local.tf
@@ -91,10 +91,10 @@ locals {
   }
 
   pdm_version = {
-    development = "0.0.81"
-    qa          = "0.0.81"
-    integration = "0.0.81"
-    preprod     = "0.0.81"
+    development = "0.0.82"
+    qa          = "0.0.82"
+    integration = "0.0.82"
+    preprod     = "0.0.82"
     production  = "0.0.81"
   }
 
