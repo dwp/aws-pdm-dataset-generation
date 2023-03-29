544b5473907cb19d0279e1068424d1e1b73f9a8c
A.H <113437236+ibI8DpR@users.noreply.github.com>
2023-03-29 12:53:45 +0100

    bump pdm version

--- local.tf
+++ local.tf
@@ -91,10 +91,10 @@ locals {
   }
 
   pdm_version = {
-    development = "0.0.87"
-    qa          = "0.0.87"
-    integration = "0.0.87"
-    preprod     = "0.0.87"
+    development = "0.0.88"
+    qa          = "0.0.88"
+    integration = "0.0.88"
+    preprod     = "0.0.88"
     production  = "0.0.86"
   }
 
