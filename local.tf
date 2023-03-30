6ae918c5c5e04130f191d82af0d517a0024ea1d6
A.H <113437236+ibI8DpR@users.noreply.github.com>
2023-03-30 14:18:17 +0100

    bump pdm version (#453)
    * bump pdm version
    
    * bumping pdm version in lower envs

--- local.tf
+++ local.tf
@@ -91,10 +91,10 @@ locals {
   }
 
   pdm_version = {
-    development = "0.0.87"
-    qa          = "0.0.87"
-    integration = "0.0.87"
-    preprod     = "0.0.87"
+    development = "0.0.89"
+    qa          = "0.0.89"
+    integration = "0.0.89"
+    preprod     = "0.0.89"
     production  = "0.0.86"
   }
 
