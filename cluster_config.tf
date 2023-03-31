582f1b9ec7cea380a4624b5697beae961113c628
rbryett <13303701+rbryett@users.noreply.github.com>
2023-03-31 12:51:25 +0100

    DTWRKS-986 propagate HCS tagging to EMR cluster (#454)
    * DTWRKS-986 propagate HCS tagging to EMR cluster
    
    * DTWRKS-986 propagate HCS tagging to EMR cluster
    
    ---------
    
    Co-authored-by: rorybryett <rory.bryett@dwp.gov.uk>

--- cluster_config.tf
+++ cluster_config.tf
@@ -8,14 +8,18 @@ resource "aws_s3_object" "cluster" {
   key    = "emr/pdm/cluster.yaml"
   content = templatefile("${path.module}/cluster_config/cluster.yaml.tpl",
     {
-      s3_log_bucket          = data.terraform_remote_state.security-tools.outputs.logstore_bucket.id
-      s3_log_prefix          = local.s3_log_prefix
-      ami_id                 = var.emr_ami_id
-      service_role           = aws_iam_role.pdm_emr_service.arn
-      instance_profile       = aws_iam_instance_profile.pdm_dataset_generator.arn
-      security_configuration = aws_emr_security_configuration.ebs_emrfs_em.id
-      emr_release_label      = var.emr_release[local.environment]
-      environment_tag_value  = local.tags.DWX_Environment
+      s3_log_bucket              = data.terraform_remote_state.security-tools.outputs.logstore_bucket.id
+      s3_log_prefix              = local.s3_log_prefix
+      ami_id                     = var.emr_ami_id
+      service_role               = aws_iam_role.pdm_emr_service.arn
+      instance_profile           = aws_iam_instance_profile.pdm_dataset_generator.arn
+      security_configuration     = aws_emr_security_configuration.ebs_emrfs_em.id
+      emr_release_label          = var.emr_release[local.environment]
+      dwx_environment_tag_value  = local.tags.DWX_Environment
+      application_tag_value      = data.aws_default_tags.provider_tags.tags.Application
+      function_tag_value         = data.aws_default_tags.provider_tags.tags.Function
+      business_project_tag_value = data.aws_default_tags.provider_tags.tags.Business-Project
+      environment_tag_value      = data.aws_default_tags.provider_tags.tags.Environment
     }
   )
   tags = {
