8ebe47482cd95765b578eac45d69183ea385a6a6
jonnym <jon.melia@cloudadept.co.uk>
2023-08-16 12:56:12 +0100

    chore:tanium sg and vpce config (#466)
    Co-authored-by: Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>

--- security_groups.tf
+++ security_groups.tf
@@ -110,43 +110,43 @@ resource "aws_security_group_rule" "ingress_internet_proxy" {
 
 
 resource "aws_security_group_rule" "pdm_host_outbound_tanium_1" {
-  description       = "PDM host outbound port 1 to Tanium"
-  type              = "egress"
-  from_port         = var.tanium_port_1
-  to_port           = var.tanium_port_1
-  protocol          = "tcp"
-  prefix_list_ids   = local.tanium_prefix[local.environment]
-  security_group_id = aws_security_group.pdm_common.id
+  description              = "PDM host outbound port 1 to Tanium"
+  type                     = "egress"
+  from_port                = var.tanium_port_1
+  to_port                  = var.tanium_port_1
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.pdm_common.id
+  source_security_group_id = data.terraform_remote_state.internal_compute.outputs.tanium_service_endpoint.sg
 }
 
 resource "aws_security_group_rule" "pdm_host_outbound_tanium_2" {
-  description       = "PDM host outbound port 2 to Tanium"
-  type              = "egress"
-  from_port         = var.tanium_port_2
-  to_port           = var.tanium_port_2
-  protocol          = "tcp"
-  prefix_list_ids   = local.tanium_prefix[local.environment]
-  security_group_id = aws_security_group.pdm_common.id
+  description              = "PDM host outbound port 2 to Tanium"
+  type                     = "egress"
+  from_port                = var.tanium_port_2
+  to_port                  = var.tanium_port_2
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.pdm_common.id
+  source_security_group_id = data.terraform_remote_state.internal_compute.outputs.tanium_service_endpoint.sg
 }
 
 resource "aws_security_group_rule" "pdm_host_inbound_tanium_1" {
-  description       = "PDM host inbound port 1 from Tanium"
-  type              = "ingress"
-  from_port         = var.tanium_port_1
-  to_port           = var.tanium_port_1
-  protocol          = "tcp"
-  prefix_list_ids   = local.tanium_prefix[local.environment]
-  security_group_id = aws_security_group.pdm_common.id
+  description              = "PDM host inbound port 1 from Tanium"
+  type                     = "ingress"
+  from_port                = var.tanium_port_1
+  to_port                  = var.tanium_port_1
+  protocol                 = "tcp"
+  security_group_id        = data.terraform_remote_state.internal_compute.outputs.tanium_service_endpoint.sg
+  source_security_group_id = aws_security_group.pdm_common.id
 }
 
 resource "aws_security_group_rule" "pdm_host_inbound_tanium_2" {
-  description       = "PDM host inbound port 2 from Tanium"
-  type              = "ingress"
-  from_port         = var.tanium_port_2
-  to_port           = var.tanium_port_2
-  protocol          = "tcp"
-  prefix_list_ids   = local.tanium_prefix[local.environment]
-  security_group_id = aws_security_group.pdm_common.id
+  description              = "PDM host inbound port 2 from Tanium"
+  type                     = "ingress"
+  from_port                = var.tanium_port_2
+  to_port                  = var.tanium_port_2
+  protocol                 = "tcp"
+  security_group_id        = data.terraform_remote_state.internal_compute.outputs.tanium_service_endpoint.sg
+  source_security_group_id = aws_security_group.pdm_common.id
 }
 
 resource "aws_security_group_rule" "egress_pdm_to_dks" {
