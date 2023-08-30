b8fa14668f9dbb0d3404c8bbae7db0e915689581
Pjay <38280665+gbpjay@users.noreply.github.com>
2023-08-30 14:19:59 +0100

    Update steps.tf (#470)
    * Update steps.tf
    
    Removed Additional Metric Step
    
    Signed-off-by: Pjay <38280665+gbpjay@users.noreply.github.com>
    
    * Update steps.yaml.tpl
    
    removed it from step file
    
    Signed-off-by: Pjay <38280665+gbpjay@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Pjay <38280665+gbpjay@users.noreply.github.com>

--- steps.tf
+++ steps.tf
@@ -224,17 +224,18 @@ resource "aws_s3_object" "flush_s3" {
   }
 }
 
-resource "aws_s3_object" "additional_metrics" {
-  bucket = data.terraform_remote_state.common.outputs.config_bucket.id
-  key    = "component/pdm-dataset-generation/additional-metrics.sh"
-  content = templatefile("${path.module}/steps/additional-metrics.sh",
-    {
-      uc_db                    = local.uc_db
-      metastore_secret_id      = data.aws_secretsmanager_secret.metastore_reader_secret.id
-      pdm_pushgateway_hostname = local.pdm_pushgateway_hostname
-    }
-  )
-  tags = {
-    Name = "additional_metrics"
-  }
-}
+// Removed additional step as it is not working as per intended design
+//resource "aws_s3_object" "additional_metrics" {
+//  bucket = data.terraform_remote_state.common.outputs.config_bucket.id
+//  key    = "component/pdm-dataset-generation/additional-metrics.sh"
+//  content = templatefile("${path.module}/steps/additional-metrics.sh",
+//    {
+//      uc_db                    = local.uc_db
+//      metastore_secret_id      = data.aws_secretsmanager_secret.metastore_reader_secret.id
+//      pdm_pushgateway_hostname = local.pdm_pushgateway_hostname
+//    }
+//  )
+//  tags = {
+//    Name = "additional_metrics"
+//  }
+//}
